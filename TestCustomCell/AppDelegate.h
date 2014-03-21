d0e824b721d368eed71ceaf8143bafed4ccebc24
hitoshi29 <hotoshi19@gmail.com>
2014-03-21 17:47:11 +0900

    Initial Commit

---
+++ TestCustomCell/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestCustomCell
+//
+//  Created by ohshiro on 2014/03/21.
+//  Copyright (c) 2014年 wappen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
